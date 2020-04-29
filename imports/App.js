737b6d611e25ca211ecc4304cf995353cfab553f
Sabrina <smin1999@gmail.com>
2020-04-29 20:51:48 +1200

    Graph working

--- imports/App.js
+++ imports/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import "./App.css"
-import moment from 'moment'
-import {Data} from './api/data'
 import CanvasJSReact from './canvasjs.react';
 let CanvasJS = CanvasJSReact.CanvasJS;
 let CanvasJSChart = CanvasJSReact.CanvasJSChart;
@@ -10,64 +8,163 @@ let CanvasJSChart = CanvasJSReact.CanvasJSChart;
 class App extends React.Component {
     constructor(props){
         super(props);
+        this.dataPoints0 = [];
+        this.dataPoints1 = [];
+        this.dataPoints2 = [];
+        this.dataPoints3 = [];
+        this.dataPoints4 = [];
+        this.dataPoints5 = [];
+        this.dataPoints6 = [];
         this.state = {
-            isToggleOn:false,
-
+            isRoom0:false,
+            isRoom1:false,
+            isRoom2:false,
+            isRoom3:false,
+            isRoom4:false,
+            isRoom5:false,
+            isRoom6:false,
         }
         this.handleClick = this.handleClick.bind(this);
-        this.handleChange = this.handleChange.bind(this);
+        this.addData = this.addData.bind(this);
     }
 
-    handleChange() {
-        let postData = document.getElementById("startdate").value
-        console.log(postData);
-
+    // Adds data points onto graph where this is called after requesting for data from Server (within Method.call)
+    addData(data) {
+        let dataPoints = [];
+        for (let i = 0; i < data.length;i++) {
+            dataPoints.push({
+                x: new Date(data[i].timestamp),
+                y: Number(data[i].temperature),
+            })
+        }
+        return dataPoints;
     }
 
-    handleClick() {
-        let info = 5;
-        Meteor.call("getInfo",info,(error,response) => {})
+    // This will sends information on the roomID to server and server will return appropriate data back 
+    // Not sure why I need the setState part (because if I delete it, the line on the graph doesn't show up)
+    handleClick(room){
+        Meteor.call("getInfo",room,(error,response) => {})
 
-        Meteor.call("retrieveData",(err,result)=>{
+        Meteor.call("retrieveData",(err,res)=>{
             if (err) {
                 console.log("error",err);
             }
-            console.log(result);
-        });
 
-        this.setState(state=> ({
-            isToggleOn: !this.state.isToggleOn
-        }));
-       
+            if (room == 0) {
+                this.dataPoints0 = this.addData(res);
+                this.setState(state=> ({
+                    isRoom0: !this.state.isRoom0
+                }));
+            }else if (room == 1){
+                this.dataPoints1 = this.addData(res);
+                this.setState(state=> ({
+                    isRoom1: !this.state.isRoom1
+                }));
+            }else if (room == 2){
+                this.dataPoints2 = this.addData(res);
+                this.setState(state=> ({
+                    isRoom2: !this.state.isRoom2
+                }));
+            }else if (room == 3){
+                this.dataPoints3 = this.addData(res);
+                this.setState(state=> ({
+                    isRoom3: !this.state.isRoom3
+                }));
+            }else if (room == 4){
+                this.dataPoints4 = this.addData(res);
+                this.setState(state=> ({
+                    isRoom4: !this.state.isRoom4
+                }));
+            }else if (room == 5){
+                this.dataPoints5 = this.addData(res);
+                this.setState(state=> ({
+                    isRoom5: !this.state.isRoom5
+                }));
+            }else if (room == 6){
+                this.dataPoints6 = this.addData(res);
+                this.setState(state=> ({
+                    isRoom6: !this.state.isRoom6
+                }));
+            }
+        });
     }
 
     render(){
+        // Set up what you need for CanvasJS to produce the graph
+        // Refer to CanvasJS for more details 
         const options = {
+            zoomEnabled:true,
             title: {
               text: "Line Chart"
             },
+            axisY:{
+              title:"Temperature (Celsius)"
+            },
+            axisX:{
+                title:"Date"
+            },
             data: [{				
-                      type: "spline",
-                      dataPoints: [
-                          { label: "Apple",  y: 10  },
-                          { label: "Orange", y: 15  },
-                         
-                      ]
-             }]
+                    type: "line",
+                    dataPoints: this.dataPoints0,
+                    legendText: "Room 0",
+                    showInLegend: true, 
+                },{
+                    type:"line",
+                    dataPoints:this.dataPoints1,
+                    legendText: "Room 1",
+                    showInLegend: true, 
+                 },{				
+                    type: "line",
+                    dataPoints: this.dataPoints2,
+                    legendText: "Room 2",
+                    showInLegend: true, 
+                },{				
+                    type: "line",
+                    dataPoints: this.dataPoints3,
+                    legendText: "Room 3",
+                    showInLegend: true, 
+                },{				
+                    type: "line",
+                    dataPoints: this.dataPoints4,
+                    legendText: "Room 4",
+                    showInLegend: true, 
+                },{				
+                    type: "line",
+                    dataPoints: this.dataPoints5,
+                    legendText: "Room 5",
+                    showInLegend: true, 
+                },{				
+                    type: "line",
+                    dataPoints: this.dataPoints6,
+                    legendText: "Room 6",
+                    showInLegend: true, 
+                },
+            
+            ]
          }
 
         return(
             <div className ="container">
                 <h1>Rooms</h1>
-                <div className="circle" id="room6" onClick={this.handleClick}></div>
-                <CanvasJSChart options = {options}/>
-              
-    
+                <div className="rooms">
+                    <div className="circle" id="room0" onClick={() => this.handleClick(0)}>0</div>
+                    <div className="circle" id="room1" onClick={() => this.handleClick(1)}>1</div>
+                    <div className="circle" id="room2" onClick={() => this.handleClick(2)}>2</div>
+                    <div className="circle" id="room3" onClick={() => this.handleClick(3)}>3</div>
+                    <div className="circle" id="room4" onClick={() => this.handleClick(4)}>4</div>
+                    <div className="circle" id="room5" onClick={() => this.handleClick(5)}>5</div>
+                    <div className="circle" id="room6" onClick={() => this.handleClick(6)}>6</div>
+                </div>
+                
+                <div className="chart">
+                    <CanvasJSChart options = {options}/>
+                </div>
+
                 <h1>Inputs</h1>
                 <div className ="inputs">
                     <div className ="start">
                         <label for="Start">Start: </label>
-                        <input type="date" id="startdate" name="startdate" max="2013-10-02" onChange={this.handleChange}/>
+                        <input type="date" id="startdate" name="startdate" max="2013-10-02"/>
                         <input type="time" id="starttime" name="starttime"/>
                     </div>
                     <div className="end">
